3fc41a1f9f4938fb0ba7f8b414faf4204a6ca0ce
unknown <avsanguenza@up.edu.ph>
2024-10-30 19:34:18 +0900

    Fixed issues

--- src/app/assets/header.tsx
+++ src/app/assets/header.tsx
@@ -1,30 +1,5 @@
 'use client'
-
-import { useState } from 'react'
-import {
-  Dialog,
-  DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel,
-} from '@headlessui/react'
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
-
 export default function Navi() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <header className="bg-white shadow mb-5">
